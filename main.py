3e2258796fdba28f374359c612887cb0b30f29d7
Sellways <78806121+Sellways@users.noreply.github.com>
2022-08-28 10:59:17 +0500

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ normal = {
     'media':    ['mp4', 'mp3', 'sfk', 'wav', 'avc', 'ogg', 'webm', 'mov', 'avi'],
     'image':    ['apng', 'png', 'gif', 'jpeg', 'svg', 'webp', 'ico', 'bmp', 'jpg'],
     'document': ['doc', 'docx', 'ppt', 'pptx', 'xlsx', 'pdf', 'txt', 'xml', 'PDF'],
-    'achive':   ['7z', 's7z', 'rar', 'zip'],
+    'archive':   ['7z', 's7z', 'rar', 'zip'],
     'torrent':  ['torrent']
 }
 
